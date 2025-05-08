1fe96b35b3e84cdff75dff9a64325f14e10d1502
Jennifer <170951762+RebeccaE23D@users.noreply.github.com>
2025-05-08 13:08:54 -0700

    Update main.cpp
    Created function to enter the player name

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+namespace std;
 
 int main() {
-
+    string player_name;
+    cout << "Enter your name:";
+    cin << player_name;
+    cout << "Great start " << player_name << ", looks like you have understood the main() function properly now!” << endl;
     return 0;
 }
